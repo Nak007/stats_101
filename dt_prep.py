7f7497fb2f7e40ed5a23d262fced154678c37894
Danusorn Sitdhirasdr <41460732+Nak007@users.noreply.github.com>
2020-02-19 21:59:06 +0700

    Update dt_prep.py

--- dt_prep.py
+++ dt_prep.py
@@ -1,8 +1,11 @@
 import pandas as pd, numpy as np, calendar
 
-mth = dict([(m.lower(),str(n)) for (n,m) in enumerate(calendar.month_abbr)])
-
 def dt9_to_dt(date):
-
-  dt = date[:2] + mth[date[2:5].lower()] + date[-4:]
-  return pd.to_datetime(dt,format='%d%m%Y')
+    
+    '''
+    Parameters : date : DATE9 i.e. 29FEB2020
+    Return : Timestamp('2020-02-29 00:00:00')
+    '''
+    mth = dict([(m.lower(),str(n)) for (n,m) in enumerate(calendar.month_abbr)])
+    dt = date[:2] + mth[date[2:5].lower()] + date[-4:]
+    return pd.to_datetime(dt,format='%d%m%Y')
